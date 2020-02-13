6a9806867edd8f6990f188907718144f06b5ac0f
Basile Masson <basilemasson2@gmail.com>
2020-02-13 16:19:43 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "cocktails/new", to: "cocktails#new", as: :new_cocktail
+  get "cocktails", to: "cocktails#index", as: :cocktails
+  get "cocktails/:id", to: "cocktails#show", as: :cocktail
+  post "cocktails", to: "cocktails#create"
+  resources :cocktails, only: [:index] do
+    resources :doses, only: [:new, :create]
+  end
+  delete "doses/:id", to: "doses#destroy"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "mango")
+Ingredient.create(name: "pepper mint")
+Ingredient.create(name: "ginger")
+
+Cocktail.create(name: "Sex on the beach")
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Bloody Mary")
+Cocktail.create(name: "Whisky sour")
+Cocktail.create(name: "Pink Martini")
+
+Dose.create(description: "1cl")
+Dose.create(description: "3cl")
+Dose.create(description: "5cl")
+Dose.create(description: "10cl")
+Dose.create(description: "20cl")
